16d4fd136662755a432a62b1305fb39732274129
mlazzell17@gmail.com <MicheSi>
2020-02-20 17:03:56 -0700

    able to pull recipes based off ingredient id

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const RecipeRouter = require('./recipes/recipe-router');
+const IngredientRouter = require('./ingredients/ingredient-router');
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(express.json());
 server.use(helmet());
 
 server.use('/api/recipes', RecipeRouter);
+server.use('/api/ingredients', IngredientRouter);
 
 server.get('/', (req, res) => {
     res.send('Server is running')
